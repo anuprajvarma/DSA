9bebe79a37b73f59d7c2009b718f4cecd45cb420
anuprajvarma <anupraj1854@gmail.com>
2022-07-04 12:23:17 +0530

    mergeTwoSortedLinkedList in practice

--- Practice/Linkedlist/mergeTwoSortedLinkedlist.cpp
+++ Practice/Linkedlist/mergeTwoSortedLinkedlist.cpp
@@ -48,6 +48,68 @@ Node *takeInput2()
     }
     return head;
 }
+Node *solve(Node *first, Node *second)
+{
+    Node *curr1 = first;
+    Node *curr2 = second;
+    Node *next1 = curr1->next;
+    Node *next2 = curr2->next;
+
+    while (next1 != NULL && curr2 != NULL)
+    {
+        if (curr1->data <= curr2->data && next1->data >= curr2->data)
+        {
+            curr1->next = curr2;
+            next2 = curr2->next;
+            curr2->next = next1;
+
+            curr1 = curr2;
+            curr2 = next2;
+        }
+        else
+        {
+            curr1 = curr1->next;
+            next1 = next1->next;
+            if (next1 == NULL)
+            {
+                curr1->next = curr2;
+                return first;
+            }
+        }
+    }
+    return first;
+}
+
+Node *mergeTwoSortedArray(Node *head1, Node *head2)
+{
+    Node *temp = head1;
+    if (head1 == NULL)
+    {
+        return head2;
+    }
+    if (head2 == NULL)
+    {
+        return head1;
+    }
+    if (head1->data < head2->data)
+    {
+        temp = solve(head1, head2);
+    }
+    else
+    {
+        temp = solve(head2, head1);
+    }
+    return temp;
+}
+
+void print(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << (*head).data << endl;
+        head = (*head).next;
+    }
+}
 
 int main()
 {
@@ -55,26 +117,6 @@ int main()
     Node *head1 = takeInput1();
     Node *head2 = takeInput2();
 
-    Node *temp1 = head1;
-    Node *temp2 = head2;
-
-    // FH = finalhead FT = finaltail
-
-    Node *FH = NULL;
-    Node *FT = NULL;
-
-    while (temp1->data != NULL && temp2->data != NULL)
-    {
-        if (temp1->data < temp2->data)
-        {
-            FH = head1;
-            FT = temp1;
-            temp1 = temp1->next;
-        }
-        else
-        {
-            FT = temp2;
-            temp2 = temp2->next;
-        }
-    }
+    Node *head = mergeTwoSortedArray(head1, head2);
+    print(head);
 }
